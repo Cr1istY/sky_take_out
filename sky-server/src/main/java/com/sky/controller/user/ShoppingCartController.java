293b8d6d63d31c06573022b65c936fb34063895b
Crist Yang <cr1st4ever@outlook.com>
2025-07-07 17:35:04 +0800

    Day 7 购物车清空

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -7,7 +7,6 @@ import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,10 +31,19 @@ public class ShoppingCartController {
         return Result.success();
     }
 
+    /**
+     * 查看购物车
+     * @return
+     */
     @GetMapping("/list")
     public Result<List<ShoppingCart>> list() {
         List<ShoppingCart> shoppingCarts = shoppingCartService.showShoppingCart();
         return Result.success(shoppingCarts);
     }
 
+    @DeleteMapping("/clean")
+    public Result clean() {
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
 }
